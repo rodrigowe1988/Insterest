8e155c4af6008922177dccf11949e1effbc311f4
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-08-25 18:56:14 -0300

    using the data of database

--- moments/src/app/services/moment.service.ts
+++ moments/src/app/services/moment.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 
 import { Moment } from '../Moment';
 import { environment } from 'src/environments/environment';
+import { Response } from '../Response';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);

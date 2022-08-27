8cbcb4f59eee6de3aaabb8ee42d07ccedab80698
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-08-27 17:48:18 -0300

    individual moment page

--- moments/src/app/app-routing.module.ts
+++ moments/src/app/app-routing.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'moments/new', component: NewMomentComponent},
-  
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'moments/new', component: NewMomentComponent },
+  { path: 'moments/:id', component: MomentComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { NewMomentComponent } from './components/pages/new-moment/new-moment.com
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MessagesComponent } from './components/messages/messages.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { MessagesComponent } from './components/messages/messages.component';
     HomeComponent,
     NewMomentComponent,
     MomentFormComponent,
-    MessagesComponent
+    MessagesComponent,
+    MomentComponent
   ],
   imports: [
     BrowserModule,

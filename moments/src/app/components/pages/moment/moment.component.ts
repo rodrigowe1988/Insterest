5fae45a00bd26013c3c5ed1dc273a48359ce3f29
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-08-27 18:50:48 -0300

    CSS moment component

--- moments/src/app/components/pages/moment/moment.component.ts
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -5,6 +5,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { MomentService } from 'src/app/services/moment.service';
 import { Moment } from 'src/app/Moment';
 
+import { environment } from 'src/environments/environment';
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-moment',
   templateUrl: './moment.component.html',
@@ -12,6 +15,10 @@ import { Moment } from 'src/app/Moment';
 })
 export class MomentComponent implements OnInit {
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(
     private momentService: MomentService,

bdeaa43a3e4bfb1b3c7a7b5d647dee1cec91238a
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-08-28 16:28:14 -0300

    finishing the moment update

--- moments/src/app/components/pages/edit-moment/edit-moment.component.ts
+++ moments/src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-edit-moment',
@@ -15,7 +16,9 @@ export class EditMomentComponent implements OnInit {
 
   constructor(
     private momentService: MomentService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private messageService: MessagesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -24,5 +27,24 @@ export class EditMomentComponent implements OnInit {
     this.momentService.getMoment(id).subscribe((item) => {
       this.moment = item.data;
     });
+  }
+
+  async editHandler(momentData: Moment) {
+    const id = this.moment.id;
+
+    const formData = new FormData();
+
+    formData.append('title', momentData.title);
+    formData.append('description', momentData.description);
+
+    if (momentData.image) {
+      formData.append('image', momentData.image);
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe();
+
+    this.messageService.add(`Moment ${momentData.title} was successfully updated!`);
+
+    this.router.navigate(['/']);
   }
 }
